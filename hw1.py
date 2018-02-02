6ea8ffff5a149ba98f0f719cd2036bdbab1e2a62
Daniel Echlin <djechlin@gmail.com>
2018-02-01 21:48:02 -0500

    rip out the code into a regular hw1.py

---
+++ hw1.py
@@ -0,0 +1,316 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.misc
+import glob
+import sys
+# you shouldn't need to make any more imports
+
+class NeuralNetwork(object):
+    """
+    Abstraction of neural network.
+    Stores parameters, activations, cached values. 
+    Provides necessary functions for training and prediction. 
+    """
+    def __init__(self, layer_dimensions, drop_prob=0.0, reg_lambda=0.0):
+        """
+        Initializes the weights and biases for each layer
+        :param layer_dimensions: (list) number of nodes in each layer
+        :param drop_prob: drop probability for dropout layers. Only required in part 2 of the assignment
+        :param reg_lambda: regularization parameter. Only required in part 2 of the assignment
+        """
+        np.random.seed(1)
+        
+        self.parameters = {}
+        self.num_layers =
+        self.drop_prob = 
+        self.reg_lambda =
+        
+        # init parameters
+        
+
+    def affineForward(self, A, W, b):
+        """
+        Forward pass for the affine layer.
+        :param A: input matrix, shape (L, S), where L is the number of hidden units in the previous layer and S is
+        the number of samples
+        :returns: the affine product WA + b, along with the cache required for the backward pass
+        """
+        
+
+    def activationForward(self, A, activation="relu"):
+        """
+        Common interface to access all activation functions.
+        :param A: input to the activation function
+        :param prob: activation funciton to apply to A. Just "relu" for this assignment.
+        :returns: activation(A)
+        """ 
+
+
+    def relu(self, X):
+
+            
+    def dropout(self, A, prob):
+        """
+        :param A: 
+        :param prob: drop prob
+        :returns: tuple (A, M) 
+            WHERE
+            A is matrix after applying dropout
+            M is dropout mask, used in the backward pass
+        """
+
+        return A, M
+
+    def forwardPropagation(self, X):
+        """
+        Runs an input X through the neural network to compute activations
+        for all layers. Returns the output computed at the last layer along
+        with the cache required for backpropagation.
+        :returns: (tuple) AL, cache
+            WHERE 
+            AL is activation of last layer
+            cache is cached values for each layer that
+                     are needed in further steps
+        """
+
+        return AL, cache
+    
+    def costFunction(self, AL, y):
+        """
+        :param AL: Activation of last layer, shape (num_classes, S)
+        :param y: labels, shape (S)
+        :param alpha: regularization parameter
+        :returns cost, dAL: A scalar denoting cost and the gradient of cost
+        """
+        # compute loss
+        
+        if self.reg_lambda > 0:
+            # add regularization
+       
+        
+        # gradient of cost
+        dAL =
+        return cost, dAL
+
+    def affineBackward(self, dA_prev, cache):
+        """
+        Backward pass for the affine layer.
+        :param dA_prev: gradient from the next layer.
+        :param cache: cache returned in affineForward
+        :returns dA: gradient on the input to this layer
+                 dW: gradient on the weights
+                 db: gradient on the bias
+        """
+
+        return dA, dW, db
+
+    def activationBackward(self, dA, cache, activation="relu"):
+        """
+        Interface to call backward on activation functions.
+        In this case, it's just relu. 
+        """
+
+        
+    def relu_derivative(self, dx, cached_x):
+
+        return dx
+
+    def dropout_backward(self, dA, cache):
+
+        return dA
+
+    def backPropagation(self, dAL, Y, cache):
+        """
+        Run backpropagation to compute gradients on all paramters in the model
+        :param dAL: gradient on the last layer of the network. Returned by the cost function.
+        :param Y: labels
+        :param cache: cached values during forwardprop
+        :returns gradients: dW and db for each weight/bias
+        """
+        gradients = {}
+        
+        for ...:
+            
+            
+            if self.drop_prob > 0:
+                #call dropout_backward
+           
+            
+        if self.reg_lambda > 0:
+            # add gradients from L2 regularization to each dW
+        
+        return gradients
+
+
+    def updateParameters(self, gradients, alpha):
+        """
+        :param gradients: gradients for each weight/bias
+        :param alpha: step size for gradient descent 
+        """
+
+    def train(self, X, y, iters=1000, alpha=0.0001, batch_size=100, print_every=100):
+        """
+        :param X: input samples, each column is a sample
+        :param y: labels for input samples, y.shape[0] must equal X.shape[1]
+        :param iters: number of training iterations
+        :param alpha: step size for gradient descent
+        :param batch_size: number of samples in a minibatch
+        :param print_every: no. of iterations to print debug info after
+        """
+        
+        for i in range(0, iters):
+            # get minibatch
+            
+            # forward prop
+
+            # compute loss
+
+            # compute gradients
+
+            # update weights and biases based on gradient
+
+            if i % print_every == 0:
+                # print cost, train and validation set accuracies
+                
+    def predict(self, X):
+        """
+        Make predictions for each sample
+        """
+
+        return y_pred
+
+    def get_batch(self, X, y, batch_size):
+        """
+        Return minibatch of samples and labels
+        
+        :param X, y: samples and corresponding labels
+        :parma batch_size: minibatch size
+        :returns: (tuple) X_batch, y_batch
+        """
+
+        return X_batch, y_batch
+
+
+# Helper functions, DO NOT modify this
+
+def get_img_array(path):
+    """
+    Given path of image, returns it's numpy array
+    """
+    return scipy.misc.imread(path)
+
+def get_files(folder):
+    """
+    Given path to folder, returns list of files in it
+    """
+    filenames = [file for file in glob.glob(folder+'*/*')]
+    filenames.sort()
+    return filenames
+
+def get_label(filepath, label2id):
+    """
+    Files are assumed to be labeled as: /path/to/file/999_frog.png
+    Returns label for a filepath
+    """
+    tokens = filepath.split('/')
+    label = tokens[-1].split('_')[1][:-4]
+    if label in label2id:
+        return label2id[label]
+    else:
+        sys.exit("Invalid label: " + label)
+
+
+# Functions to load data, DO NOT change these
+
+def get_labels(folder, label2id):
+    """
+    Returns vector of labels extracted from filenames of all files in folder
+    :param folder: path to data folder
+    :param label2id: mapping of text labels to numeric ids. (Eg: automobile -> 0)
+    """
+    files = get_files(folder)
+    y = []
+    for f in files:
+        y.append(get_label(f,label2id))
+    return np.array(y)
+
+def one_hot(y, num_classes=10):
+    """
+    Converts each label index in y to vector with one_hot encoding
+    """
+    y_one_hot = np.zeros((y.shape[0], num_classes))
+    y_one_hot[y] = 1
+    return y_one_hot.T
+
+def get_label_mapping(label_file):
+    """
+    Returns mappings of label to index and index to label
+    The input file has list of labels, each on a separate line.
+    """
+    with open(label_file, 'r') as f:
+        id2label = f.readlines()
+        id2label = [l.strip() for l in id2label]
+    label2id = {}
+    count = 0
+    for label in id2label:
+        label2id[label] = count
+        count += 1
+    return id2label, label2id
+
+def get_images(folder):
+    """
+    returns numpy array of all samples in folder
+    each column is a sample resized to 30x30 and flattened
+    """
+    files = get_files(folder)
+    images = []
+    count = 0
+    
+    for f in files:
+        count += 1
+        if count % 10000 == 0:
+            print("Loaded {}/{}".format(count,len(files)))
+        img_arr = get_img_array(f)
+        img_arr = img_arr.flatten() / 255.0
+        images.append(img_arr)
+    X = np.column_stack(images)
+
+    return X
+
+def get_train_data(data_root_path):
+    """
+    Return X and y
+    """
+    train_data_path = data_root_path + 'train'
+    id2label, label2id = get_label_mapping(data_root_path+'labels.txt')
+    print(label2id)
+    X = get_images(train_data_path)
+    y = get_labels(train_data_path, label2id)
+    return X, y
+
+def save_predictions(filename, y):py
+    """
+    Dumps y into .npy file
+    """
+    np.save(filename, y)
+
+
+# Load the data
+data_root_path = '/path/to/extracted/data/cifar10-hw1/'
+X_train, y_train = get_train_data(data_root_path) # this may take a few minutes
+X_test = get_images(data_root_path + 'test')
+print('Data loading done')
+
+# Part 1
+
+layer_dimensions = [X_train.shape[0], ..., 10]  # including the input and output layers
+NN = NeuralNetwork(layer_dimensions)
+NN.train(X_train, y_train, iters=, alpha=, batch_size=, print_every=)
+
+# y_predicted = NN.predict(X_test)
+save_predictions('ans1-uni', y_predicted)
+
+# test if your numpy file has been saved correctly
+loaded_y = np.load('ans1-uni.npy')
+print(loaded_y.shape)
+loaded_y[:10]
