f89943eeed38e34eadb5160fe3c0685b0f87c6f0
mp3242 <mp3242@columbia.edu>
2018-02-06 21:29:30 -0500

    actual updated affinebackwards

--- hw1.py
+++ hw1.py
@@ -9,76 +9,328 @@ from helpers import *
 
 # you shouldn't need to make any more imports
 
-# Functions to load data, DO NOT change these
+class NeuralNetwork(object):
+    """
+    Abstraction of neural network.
+    Stores parameters, activations, cached values.
+    Provides necessary functions for training and prediction.
+    """
+    def __init__(self, layer_dimensions, drop_prob=0.0, reg_lambda=0.0,
+            weights=None, biases=None, activation=None):
+        """
+        Initializes the weights and biases for each layer
+        :param layer_dimensions: (list) number of nodes in each layer
+        :param drop_prob: drop probability for dropout layers. Only required in part 2 of the assignment
+        :param reg_lambda: regularization parameter. Only required in part 2 of the assignment
+        """
+        seed = 1
+        np.random.seed(seed)
 
-def get_labels(folder, label2id):
-    """
-    Returns vector of labels extracted from filenames of all files in folder
-    :param folder: path to data folder
-    :param label2id: mapping of text labels to numeric ids. (Eg: automobile -> 0)
-    """
-    files = get_files(folder)
-    y = []
-    for f in files:
-        y.append(get_label(f,label2id))
-    return np.array(y)
+        self.parameters = {}
+        self.num_layers = len(layer_dimensions)
+        self.drop_prob = drop_prob
+        self.reg_lambda = reg_lambda
+        self.activation = activation
+        self.relu_v = np.vectorize(self.relu)
+        self.relud_v = np.vectorize(self.relu_derivative)
+        # init parameters
+        
+        #idx is the index of val - 1, because idx starts at zero, despite enumerating starting
+        #at layer_dimensions[1:]. So idx points to the previous layer.
+        self.parameters['weights'] = [np.random.randn(layer_dimensions[idx], val)
+            for idx, val in enumerate(layer_dimensions[1:])]
 
-def one_hot(y, num_classes=10):
-    """
-    Converts each label index in y to vector with one_hot encoding
-    """
-    y_one_hot = np.zeros((y.shape[0], num_classes))
-    y_one_hot[y] = 1
-    return y_one_hot.T
+        self.parameters['biases'] = [np.random.randn(n, 1)
+            for n in layer_dimensions[1:]]
 
-def get_label_mapping(label_file):
-    """
-    Returns mappings of label to index and index to label
-    The input file has list of labels, each on a separate line.
-    """
-    with open(label_file, 'r') as f:
-        id2label = f.readlines()
-        id2label = [l.strip() for l in id2label]
-    label2id = {}
-    count = 0
-    for label in id2label:
-        label2id[label] = count
-        count += 1
-    return id2label, label2id
+    def affineForward(self, A, W, b):
+        """
+        Forward pass for the affine layer.
+        :param A: input matrix, shape (L, S), where L is the number of hidden units in the previous layer and S is
+        the number of samples
+        :returns: the affine product WA + b, along with the cache required for the backward pass
+        """
+        #or maybe join b into W
+        
+        Z = W.transpose().dot(A) + b
+        cache = (A,W,b,Z)
 
-def get_images(folder):
-    """
-    returns numpy array of all samples in folder
-    each column is a sample resized to 30x30 and flattened
-    """
-    files = get_files(folder)
-    images = []
-    count = 0
-    
-    for f in files:
-        count += 1
-        if count % 10000 == 0:
-            print("Loaded {}/{}".format(count,len(files)))
-        img_arr = get_img_array(f)
-        img_arr = img_arr.flatten() / 255.0
-        images.append(img_arr)
-    X = np.column_stack(images)
+        print("-affineForward()")
+        print("W: " + str(W.shape))
+        print("A: " + str(A.shape))
+        print("b: " + str(b.shape))
+        print("Z: %s" % str(Z.shape))
+        
+        return Z, cache
 
-    return X
+    def activationForward(self, A, activation="relu"):
+        """
+        Common interface to access all activation functions.
+        :param A: input to the activation function
+        :param prob: activation funciton to apply to A. Just "relu" for this assignment.
+        :returns: activation(A)
+        """
+        if self.activation is not None:
+            return self.activation(A)
 
-def get_train_data(data_root_path):
-    """
-    Return X and y
-    """
-    train_data_path = data_root_path + 'train'
-    id2label, label2id = get_label_mapping(data_root_path+'labels.txt')
-    print(label2id)
-    X = get_images(train_data_path)
-    y = get_labels(train_data_path, label2id)
-    return X, y
+        print("Apost: %s " % str(A.shape))
+        return self.relu_v(A)
 
-def save_predictions(filename, y):
-    """
-    Dumps y into .npy file
-    """
-    np.save(filename, y)
+    def relu(self, X):
+        return np.maximum(0,X)
+
+    def dropout(self, A, prob):
+        """
+        :param A:
+        :param prob: drop prob
+        :returns: tuple (A, M)
+            WHERE
+            A is matrix after applying dropout
+            M is dropout mask, used in the backward pass
+        """
+
+        return A, M
+
+    def forwardPropagation(self, X):
+        """
+        Runs an input X through the neural network to compute activations
+        for all layers. Returns the output computed at the last layer along
+        with the cache required for backpropagation.
+        :returns: (tuple) AL, cache
+            WHERE
+            AL is activation of last layer
+            cache is cached values for each layer that
+                     are needed in further steps
+        """
+        print("-forwardPropagation()")
+        cache = []
+        A = X
+        W = self.parameters['weights']
+        b = self.parameters['biases']
+
+        # range(0, 3) = (0, 1, 2)
+        # two fewer computation than layers (last cycle will be softmax)
+        for l in range(0, self.num_layers - 2):
+            print("layer" + str(l))
+            Z, c = self.affineForward(A, W[l], b[l])
+            A = self.activationForward(Z)
+            cache.append(c)
+        
+        Z, c = self.affineForward(A, W[self.num_layers-2], b[self.num_layers-2])
+        cache.append(c)
+
+        #softmax
+        A_exp = np.exp(Z)
+        AL = A_exp / np.sum(A_exp,axis=0)
+        
+        print("---=-=-=--=-=-=-")
+        print(len(cache))
+        return AL, cache
+
+    def costFunction(self, AL, y):
+        """
+        :param AL: Activation of last layer, shape (num_classes, S)
+        :param y: labels, shape (S)
+        :param alpha: regularization parameter
+        :returns cost, dAL: A scalar denoting cost and the gradient of cost
+        """
+        # compute loss
+        # log loss
+        y_i = 0
+        cost = 0
+        for sample in AL.T:
+            #if label = node #, then yTrue = 1
+            for i, node in enumerate(sample):
+                yTrue = 1 if y[y_i] == i else 0
+                cost += -yTrue*np.log(sample[i]) - (1 - yTrue)*np.log(1 - sample[i])
+            y_i+=1
+        if self.reg_lambda > 0:
+            pass
+            # add regularization
+            # TODO
+
+        cost /= AL.shape[1]
+
+        # gradient of cost #just subtract 1 from the correct class
+        
+        dAL = AL
+        y_i=0
+        for j in range(0, AL.shape[1]): #cols - samples
+            for i in range(0, AL.shape[0]): #rows - nodes
+                yTrue = 1 if y[y_i] == i else 0
+                dAL[i,j] -= yTrue
+            y_i+=1
+
+        #average over all samples - DONT DO
+        #dAL = AL.sum(axis=1)/AL.shape(1)
+
+        return cost, dAL
+
+    def affineBackward(self, dA_prev, cache):
+        """
+        Backward pass for the affine layer.
+        :param dA_prev: gradient from the next layer.
+        :param cache: cache returned in affineForward
+        :returns dA: gradient on the input to this layer
+                 dW: gradient on the weights
+                 db: gradient on the bias
+        """
+        #migrate some stuff to activationBackward
+        g_prime = self.relud_v
+        
+        A, W, b, Z_r = cache[0], cache[1], cache[2], cache[3]
+        
+        print("<< W:       " + str(W.shape))
+        print("<< dA_prev: " + str(dA_prev.shape))
+        print("<< Z_r:     " + str(Z_r.shape))
+        print("<< g':      " + str(g_prime(0,Z_r).shape))
+        
+#        dZ
+        
+        dA = np.dot(W,dA_prev) #g_prime(0,Z_r))
+        dW = np.dot(dA_prev,A.T) # g_prime(0,Z_r)
+        db = 0 ######~~~~ TODO ~~~~~#####
+
+        return dA, dW, db
+
+    def activationBackward(self, dA, cache, activation="relu"):
+        """
+        Interface to call backward on activation functions.
+        In this case, it's just relu.
+        """
+        pass
+
+
+    def relu_derivative(self, dx, cached_x):
+        #don't need param: dx
+        dx = 1 if cached_x >= 0 else 0
+        return dx
+
+    def dropout_backward(self, dA, cache):
+
+        return dA
+
+    def backPropagation(self, dAL, Y, cache):
+        """
+        Run backpropagation to compute gradients on all paramters in the model
+        :param dAL: gradient on the last layer of the network. Returned by the cost function.
+        :param Y: labels
+        :param cache: cached values during forwardprop
+        :returns gradients: dW and db for each weight/bias
+        """
+
+        """
+        Key math:
+        Setting from notes:
+        Use r instead of l for layer
+        Z^r = (W^r)^T * A^(r-1) = affine layer
+        A^r = g^r(Z^r) = activated layer
+        W^r : layer r-1 -> layer r
+        g^r is probably relu, but you can add a g^final = softmax step
+
+        Problem: given dL/dA^r, compute dL/A^r and DL/dW^r. We only need to return
+        dL/dW^r, bu computing dL/dA^r enables recursion.
+
+        Implement this update:
+        Temp: Z^r = g(A^(r-1))
+        Set: dL/dA^(r-1) = (W^r)^T * dL/dA^r * g'(Z^r) (note g' not g)
+        Set: dL/dW^r = dL/dA^r * g'(Z^r) * (A^(r-1)^)T (note g' not g)
+
+        """
+        #gradients = []
+        gradients = {}
+
+        #hopefully list length is correct
+        gradients['dW'] = [0] * (self.num_layers - 1) 
+        gradients['db'] = [0] * (self.num_layers - 1)
+        
+        dL_dA_rprev = dAL
+
+        print("alskjfalksdjfs   " + str(len(cache)))
+        for c in cache:
+            for i in c:
+                print (i.shape,)
+            print ("\n")
+        
+        # start with last layer, note that range(3,-1,-1) == (3, 2, 1, 0)
+        for r in range(self.num_layers - 2, -1, -1):
+            dL_dA_rprev, dL_dW_r, dL_db_r = self.affineBackward(dL_dA_rprev, cache[r])
+
+            gradients['dW'][r] = dL_dW_r
+            gradients['db'][r] = dL_db_r
+            #TODO: bias
+
+            if self.drop_prob > 0:
+                #call dropout_backward
+                pass
+
+        if self.reg_lambda > 0:
+            # add gradients from L2 regularization to each dW
+            pass
+
+        return gradients
+
+
+    def updateParameters(self, gradients, alpha):
+        """
+        :param gradients: gradients for each weight/bias
+        :param alpha: step size for gradient descent
+        """
+        #assuming gradients have been averaged across samples already
+        for i,dW in enumerate(gradients['dW']):
+            deltaW = dW * alpha
+            print(deltaW.shape)
+            print(self.parameters['weights'][i].shape)
+            self.parameters['weights'][i] -= deltaW.transpose()
+        for i,db in enumerate(gradients['db']):
+            deltab = db * alpha
+            self.parameters['biases'][i] -= deltab
+
+    def train(self, X, y, iters=1000, alpha=0.0001, batch_size=100, print_every=100):
+        """
+        :param X: input samples, each column is a sample
+        :param y: labels for input samples, y.shape[0] must equal X.shape[1]
+        :param iters: number of training iterations
+        :param alpha: step size for gradient descent
+        :param batch_size: number of samples in a minibatch
+        :param print_every: no. of iterations to print debug info after
+        """
+
+        for i in range(0, iters):
+            # get minibatch
+            X_batch, y_batch = self.get_batch(X, y, batch_size)
+            # forward prop
+            AL, cache = self.forwardPropagation(X_batch)
+            # compute loss
+            cost, dAL = self.costFunction(AL, y_batch)
+            # compute gradients
+            gradients = self.backPropagation(dAL, y_batch, cache)
+            # update weights and biases based on gradient
+            self.updateParameters(gradients, alpha)
+            if i % print_every == 0:
+                print("Cost: " + str(cost))
+                # print cost, train and validation set accuracies
+
+    def predict(self, X):
+        """
+        Make predictions for each sample
+        """
+        return self.forwardPropagation(X)
+
+    def get_batch(self, X, y, batch_size):
+        """
+        Return minibatch of samples and labels
+
+        :param X, y: samples and corresponding labels
+        :parma batch_size: minibatch size
+        :returns: (tuple) X_batch, y_batch
+        """
+        
+        sample = random.sample(range(X.shape[1]), batch_size)
+
+        #Assuming X and y are numpy arrays
+        X_batch = X[:,sample]
+        y_batch = y[sample]
+        return X_batch, y_batch
+
+
